10ad774d1b34095b6d9cb61472616536d1f26ca4
afeibukun <afeibukun@gmail.com>
2019-10-21 22:05:59 +0100

    Registration section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/layout/Header';
 import Home from './components/home';
+import Register from './components/auth/Register';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </Router>
   );
