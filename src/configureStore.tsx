84ca698112e7194bd2c6247ee9e0d39ec9c9aefa
Daniel Ale <danielale9291@gmail.com>
2019-10-22 12:04:00 +0100

    Paying with redux persist for hooks

--- src/configureStore.tsx
+++ src/configureStore.tsx
@@ -1,4 +1,7 @@
 import React, { createContext, useReducer } from 'react';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //
+import { PersistGate } from 'redux-persist/integration/react';
 import { IState } from './interfaces';
 import { reducer } from './reducers';
 
@@ -11,13 +14,23 @@ const initialState: IState = {
   },
 };
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const Store = createContext<IState | any>(initialState);
+export const persistor = persistStore(Store);
 
 export const StoreProvider = (props: any) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(persistedReducer, initialState);
   return (
     <Store.Provider value={{ state, dispatch }}>
-      {props.children}
+      <PersistGate loading={null} persistor={persistor}>
+        {props.children}
+      </PersistGate>
     </Store.Provider>
   );
 };
